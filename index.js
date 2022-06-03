d5943c83504fd9bdf1c1281f34725a89f66d366e
samuelClo <cloarec.samuel@hotmail.fr>
2022-06-03 16:30:38 +0200

    k

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
   console.log('Yes man')
+  res.end('Hello World');
+  
 });
 
 server.listen(port, hostname, () => {
