a210f2a38e49e4c9e96151acf6897748b42e62d6
samuelClo <cloarec.samuel@hotmail.fr>
2022-06-03 16:35:32 +0200

    add lauch test

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+  console.log('walou - ')
+})
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  console.log('Yes man')
-  res.end('Hello World');
-  
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
